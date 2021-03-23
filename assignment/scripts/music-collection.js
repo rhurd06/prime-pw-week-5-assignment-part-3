f16e4424d0870cf8bc0213bbd20bfdb3222d6852
Robyn Noble <robynhurd09@gmail.com>
2021-03-23 10:49:13 -0500

    albums 0-3 added

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,7 +2,7 @@ console.log('***** Music Collection *****')
 
 let collection = [ ]
 function addToCollection( ){
-  collection.push (album0);
+
   return collection;
 }
 let album0 ={
@@ -10,9 +10,23 @@ let album0 ={
   artist: 'TLC',
   yearPublished: 1999,
 }
-console.log(addToCollection());
+collection.push(album0);
 let album1 ={
   title: 'Some Hearts',
   artist: 'Carrie Underwood',
   yearPublished: 2005,
 }
+collection.push(album1);
+let album2 ={
+  title: 'Carnival Ride',
+  artist: 'Carrie Underwood',
+  yearPublished: 2007,
+}
+collection.push(album2);
+let album3 ={
+  title: 'Tailgates & Tanlines',
+  artist: 'Luke Bryan',
+  yearPublished: 2011,
+}
+collection.push(album3)
+console.log( addToCollection());
