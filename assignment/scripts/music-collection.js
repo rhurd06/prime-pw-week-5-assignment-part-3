02ef3847141b2052c6484eb16fcf25d23d926319
Robyn Noble <robynhurd09@gmail.com>
2021-03-26 12:49:22 -0500

    search working

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,47 +1,16 @@
 console.log('***** Music Collection *****')
-
 let collection = [ ]
-function addToCollection( ){
-
+function addToCollection(title, artist, yearPublished){
+  let album = {title: title, artist: artist, yearPublished: yearPublished};
+  collection.push(album);
   return collection;
 }
-let album0 ={
-  title: 'FanMail',
-  artist: 'TLC',
-  yearPublished: 1999,
-}
-collection.push(album0);
-let album1 ={
-  title: 'Some Hearts',
-  artist: 'Carrie Underwood',
-  yearPublished: 2005,
-}
-collection.push(album1);
-let album2 ={
-  title: 'Carnival Ride',
-  artist: 'Carrie Underwood',
-  yearPublished: 2007,
-}
-collection.push(album2);
-let album3 ={
-  title: 'Tailgates & Tanlines',
-  artist: 'Luke Bryan',
-  yearPublished: 2011,
-}
-collection.push(album3);
-let album4 ={
-  title: 'Crash My Party',
-  artist: 'Luke Bryan',
-  yearPublished: 2013,
-}
-collection.push(album4);
-let album5 ={
-  title: 'Doo-Wops & Hooligans',
-  artist: 'Bruno Mars',
-  yearPublished: 2010,
-}
-collection.push(album5);
-console.log( addToCollection());
+addToCollection('FanMail', 'TLC', '1999');
+addToCollection('Some Hearts', 'Carrie Underwood', '2005');
+addToCollection('Carnival Ride', 'Carrie Underwood', '2007');
+addToCollection('Tailgates & Tanlines', 'Luke Bryan', '2011');
+addToCollection('Crash My Party', 'Luke Bryan', '2013');
+addToCollection('Doo-Wops & Hooligans', 'Bruno Mars', '2010');
 console.log(collection[0]);
 console.log(collection[1]);
 console.log(collection[2]);
@@ -49,3 +18,40 @@ console.log(collection[3]);
 console.log(collection[4]);
 console.log(collection[5]);
 console.log(collection);
+function showCollection(collection){
+  console.log('The number of items in this array are:', collection.length);
+    for (let album of collection){
+      console.log('The song titles in my array are:', album.title);
+      console.log(album.title, 'by', album.artist, 'published in', album.yearPublished)
+    }
+} //end showCollection
+showCollection(collection);
+function findByArtist( artist){
+  let carrieUnderwoodSongs = [ ]
+    for (let album of collection){
+      if (album.artist === artist ){
+      //console.log();})
+      carrieUnderwoodSongs.push( album );
+    }
+  }//end for of loop
+  return carrieUnderwoodSongs;
+}//end findByArtist
+console.log(findByArtist('Carrie Underwood'));
+console.log(findByArtist('Luke Bryan'));
+console.log(findByArtist('Mary'));
+function search( artist, yearPublished ){
+  let lukeBryanSongs= [ ]
+    for (let album of collection){
+      if (album.artist === artist && album.yearPublished === yearPublished){
+        //console.log();})
+        lukeBryanSongs.push (album );
+      }
+    }//end for of loop
+    if (artist === undefined ){
+      return collection;
+    }
+    return lukeBryanSongs;
+}//end search
+console.log(search('Luke Bryan', '2013'));
+console.log(search('Luke Bryan', '2010'));
+console.log(search());
